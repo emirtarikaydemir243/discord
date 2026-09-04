0f98a3d3979d9944ea7fbc562fa14e803763e709
emirtarikaydemir243 <emirtarikaydemir01@gmail.com>
2026-09-04 19:02:21 +0300

    Convert bot to use commands and add features
    Refactor Discord bot to use commands extension and add new commands.

--- bot.py
+++ bot.py
@@ -1,30 +1,76 @@
-from logic import gen_pass
+import random
 import discord
+import os
+import requests
+from discord.ext import commands
 
-# ayricaliklar (intents) değişkeni botun ayrıcalıklarını depolayacak
 intents = discord.Intents.default()
-# Mesajları okuma ayrıcalığını etkinleştirelim
 intents.message_content = True
-# client (istemci) değişkeniyle bir bot oluşturalım ve ayrıcalıkları ona aktaralım
-client = discord.Client(intents=intents)
 
-@client.event
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
 async def on_ready():
-    print(f'{client.user} olarak giriş yaptık.')
+    print(f'{bot.user} olarak giriş yaptık')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$merhaba'):
-        await message.channel.send("Selam!") 
-    elif message.content.startswith('$bye'):
-        await message.channel.send("\U0001f642")
-    elif message.content.startswith('$password'):
-        password = gen_pass(12)
-        await message.channel.send(f"İste rastgele bir şifre: {password}")
-    else:
-        await message.channel.send(message.content)
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+
+@bot.command()
+async def mem(ctx):
+    files=os.listdir('images')
+    selected = random.choice(files)
+
+    with open(f'images/{selected}', 'rb') as f:
+        # Dönüştürülen Discord kütüphane dosyasını bu değişkende saklayalım!
+        picture = discord.File(f)
+   # Daha sonra bu dosyayı bir parametre olarak gönderebiliriz!
+    await ctx.send(file=picture)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''duck komutunu çağırdığımızda, get_duck_image_url fonksiyonunu çağırır.'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+def get_dog_image_url():    
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('dog')
+async def dog(ctx):
+    '''dog komutunu çağırdığımızda, get_dog_image_url fonksiyonunu çağırır.'''
+    image_url = get_dog_image_url()
+    await ctx.send(image_url)
+
+@bot.command()
+async def commands(ctx):
+    await ctx.send(f'İşte kullanabileceğiniz bazı komutlar : !hello , !heh , !repeat , !choose , !mem , !duck , !dog , !commands')
 
     
-client.run("tokeninizi girin ...")
+bot.run("Tokeninizi Girin !")
