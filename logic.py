54bfa436069a275a0077d673e13b7556b3f6dd02
emirtarikaydemir243 <emirtarikaydemir01@gmail.com>
2026-09-04 19:02:50 +0300

    Add emoji generator function

--- logic.py
+++ logic.py
@@ -6,3 +6,7 @@ def gen_pass(pass_length):
     for i in range(pass_length):
         password += random.choice(elements)
     return password
+
+def emoji_olusturucu():
+    emoji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
+    return random.choice(emoji)
